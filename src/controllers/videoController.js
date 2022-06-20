15e9839709f35c0b36b7254de2dde37907904a76
hi-rachel <rachel.uiux@gmail.com>
2022-06-20 16:45:17 +0900

    error solved

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,8 @@
 export const trending = (req, res) =>res.render("home");
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
-export const upload = (req, res) => res.send("Upload");
+export const upload = (req, res) => res.send("upload");
+export const search = (req, res) => res.send("search");
 export const deleteVideo = (req, res) => {
     return res.send("Delete Video");
 };
