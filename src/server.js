9e4e1ec1e3076604e537f12b8b82e09c10669259
hi-rachel <rachel.uiux@gmail.com>
2022-06-21 22:27:29 +0900

    mixins + Array Database

--- src/server.js
+++ src/server.js
@@ -4,29 +4,18 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 5000;
+const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/user", userRouter);
-
-const home = (req, res) => {
-    console.log("I will respond.")
-    return res.send("hello");
-};
-
-const login = (req, res) => {
-    return res.send("login");
-};
 
 const handleListening = () =>
     console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
- 
